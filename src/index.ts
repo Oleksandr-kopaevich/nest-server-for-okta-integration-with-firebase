603289f12c59a308264bb1ff373cfe3ba187e8d1
Oleksandr Kopaevich <oleksandr.kopaevich@altexsoft.com>
2021-07-29 21:23:16 +0300

    integrate nestjs with firebase functions

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import { AppModule } from './app.module';
+  
+const expressServer = express();
+ 
+const createNestServer = async (expressInstance): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+ 
+  await app.init();
+};
+ 
+export const api = functions.region('europe-west1')
+  .https.onRequest(async (request, response) => {
+    await createNestServer(expressServer);
+    expressServer(request, response);
+  });
