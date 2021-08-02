5fc283414ac7898111b31f9fa39ff844722812d3
Oleksandr Kopaevich <oleksandr.kopaevich@altexsoft.com>
2021-08-02 12:18:39 +0300

    setup cors for prod

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import * as cors from 'cors';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './app.module';
 
 const expressServer = express();
+expressServer.use(
+  cors({
+    origin: 'https://explore-firebase-37b42.web.app',
+    allowedHeaders: '*',
+  }),
+);
 
 const createNestServer = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
   );
-  
+
   await app.init();
 };
 
